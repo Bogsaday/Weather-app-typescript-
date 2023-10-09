84ec6247f4c44f0c123eadf5b05578e1a1f80a5d
sidiik <sidiikpro@gmail.com>
2023-10-09 19:08:53 +0300

    done our first ai project

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+// declartions
+const url = 'https://chatgpt-api7.p.rapidapi.com/ask';
+
+const inputEl = document.querySelector('.inputEl') as HTMLInputElement;
+const btnGenerate = document.querySelector('.btnGenerate') as HTMLButtonElement;
+const responseEl = document.querySelector(
+  '.responseEl'
+) as HTMLParagraphElement;
+
+async function generateResponse(question: string) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': 'b770f195e9msh2a64ed1c004be3bp198e36jsnd38945b2bec6',
+      'X-RapidAPI-Host': 'chatgpt-api7.p.rapidapi.com',
+    },
+    body: JSON.stringify({
+      query: question,
+    }),
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  if (data.response) {
+    responseEl.textContent = data.response;
+  } else {
+    responseEl.textContent = 'Failed to load the response.';
+  }
+}
+
+// events
+
+btnGenerate.addEventListener('click', () => {
+  const inputValue = inputEl.value.trim();
+
+  if (inputValue.length < 1) {
+    alert('You need to enter a question.');
+  } else {
+    responseEl.textContent = 'Generating your response...';
+    generateResponse(inputValue);
+  }
+});
